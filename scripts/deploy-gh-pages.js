b3d6b4deaf785b7479de607b5bfcefbd1e5afdbc
alu0100892833 <alu0100892833@ull.edu.es>
2017-03-04 19:00:40 +0000

    Automatizado el despliegue en GitHub Pages

---
+++ scripts/deploy-gh-pages.js
@@ -0,0 +1,10 @@
+var CMDDESC = "Deploying on Github Pages";
+
+var repositorio = require('../package.json').repository.url;
+var ghpages = require('gh-pages');
+ghpages.publish('./gh-pages', {
+  repo: repositorio,
+  logger: function(m) {
+	   console.error(m);
+  }
+});
